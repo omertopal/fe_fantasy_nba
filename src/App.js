86dda7d1ce4ef4b569e3798ed93b0f641d2ca598
Omer TOPAL <omertopalmail@gmail.com>
2019-02-04 23:04:48 +0300

    Axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Table, Button} from 'reactstrap';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    players : []
+  }
+
+  componentWillMount(){
+    axios.get('http://10.0.75.1:8084/nba/players/myPlayers').then((response) => {
+      this.setState({
+        players : response.data
+      })
+    });
+  }
+
+
+
   render() {
+    let players = this.state.players.map((player) => {
+      return (
+        <tr key={player.id}>
+          <td>{player.id}</td>
+          <td>{player.name}</td>
+          <td>{player.avgPts}</td>
+          <td>PG:{player.isPG} SG:{player.isSG}</td>
+          <td>
+            <Button color="success" size="sm" className="mr-2">Edit</Button>
+            <Button color="danger" size="sm">Delete</Button>
+          </td>
+        </tr>
+      )
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Avg Pts</th>
+              <th>Skills</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players}
+          </tbody>
+        </Table>
       </div>
     );
   }
